cb43947e88cc8998612004847ffd5155f3617cc2
unknown <stam296@gmail.com>
2023-06-27 23:55:46 -0500

    created functions to add to the hierarchical inquirer prompts for follow up questions

--- index.js
+++ index.js
@@ -20,33 +20,119 @@ const tablePrompt = {
 
 inquirer
     .prompt(tablePrompt).then((response) => {
-        // const {choice} = response;
         console.log(response.display);
         if (response.display == "View all departments") {
-            console.log("here");
             db.query('SELECT * FROM department', function(err, results) {
                 console.table(results);
             });
         }
         else if (response.display === "View all roles") {
-
+            db.query('SELECT * FROM employee_role', function(err, results) {
+                console.table(results);
+            });
         }
         else if (response.display === "View all employees") {
-
+            db.query('SELECT * FROM employee', function(err, results) {
+                console.table(results);
+            });
         }
         else if (response.display === "Add a department") {
-
+            addDepartment();
         }
         else if (response.display === "Add a role") {
-
+            addRole();
         }
         else if (response.display === "Add an employee") {
-
+            addEmployee();
         }
         else if (response.display === "Update an employee role") {
-
+            updateEmployee();
         }
         else {
             console.log("Need response");
         }
     });
+
+function addDepartment() {
+    inquirer
+        .prompt({
+            type: 'input',
+            name: 'department',
+            message: 'What is the name of the department?'
+        })
+        .then((response) =>
+        console.log(response)
+        );
+};
+
+function addRole() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'role',
+                message: 'What is the name of the role?'
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'What is the salary of the role?'
+            },
+            {
+                type: 'input',
+                name: 'department',
+                message: 'Which department does the role belong to?'
+            },
+        ])
+        .then((response) =>
+        console.log(response)
+        );
+};
+
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'firstname',
+                message: 'What is the employees first name?'
+            },
+            {
+                type: 'input',
+                name: 'lastname',
+                message: 'What is the employees last name?'
+            },
+            {
+                type: 'input',
+                name: 'role',
+                message: 'What is the employees role?'
+            },
+            {
+                type: 'input',
+                name: 'manager',
+                message: 'Who is the employees manager'
+            },
+        ])
+        .then((response) =>
+        console.log(response)
+        );
+};
+
+function updateEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'update',
+                message: 'Which employees role do you want to update?'
+            },
+            {
+                type: 'input',
+                name: 'role',
+                message: 'Which role do you want to assign the selected employee?'
+            },
+        ])
+        .then((response) =>
+        console.log(response)
+        );
+};
